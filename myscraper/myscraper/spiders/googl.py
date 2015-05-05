24e1128a4e798e7e6a9511b4a8bc2930188e41bf
Nicholas Pike <mavr1k@gmail.com>
2015-05-05 10:16:51 -0700

    initial commit

---
+++ myscraper/myscraper/spiders/googl.py
@@ -0,0 +1,27 @@
+import scrapy
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import Selector
+
+class GooglSpider(CrawlSpider):
+    name = "GooglSpider"
+    DOWNLOAD_DELAY = 0.25
+    allowed_domains = ["developers.google.com"]
+    start_urls = [
+        "https://developers.google.com"
+    ]
+
+    rules = ( Rule (SgmlLinkExtractor(allow=("", ),),
+                callback="parse_items",  follow= True),
+    )
+
+    def __init__(self, session_id=-1, *args, **kwargs):
+        super(GooglSpider, self).__init__(*args, **kwargs)
+
+    # def parse(self, response):
+    #     if "script_foot.js" in response.body:
+    #         print "Found easter"
+    def parse_items(self, response):
+        if "goo.gl" in response.body:
+            print response.url + " found"
+        
